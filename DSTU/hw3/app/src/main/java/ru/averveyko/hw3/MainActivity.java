004f2e79501c35f9b65b727105b1ee2d90ab24c3
Verveyko Alexander <averveiko@gmail.com>
2020-11-14 12:56:43 +0300

    hw3 realise

--- DSTU/hw3/app/src/main/java/ru/averveyko/hw3/MainActivity.java
+++ DSTU/hw3/app/src/main/java/ru/averveyko/hw3/MainActivity.java
@@ -2,47 +2,100 @@ package ru.averveyko.hw3;
 
 import android.os.Bundle;
 import android.os.SystemClock;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.Chronometer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    long timeElapsed;
+    private Chronometer chronometer;
+
+    private long timeElapsed;
+    private boolean isRunning = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.v("keash", "onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Chronometer chronometer = findViewById(R.id.chronometer);
+        chronometer = findViewById(R.id.chronometer);
 
         Button butStart = findViewById(R.id.but_start);
         Button butStop = findViewById(R.id.but_stop);
         Button butReset = findViewById(R.id.but_reset);
 
-        chronometer.setOnChronometerTickListener(listener -> {
-            timeElapsed = SystemClock.elapsedRealtime() - chronometer.getBase();
-        });
+        if (savedInstanceState != null
+                && savedInstanceState.containsKey("timeElapsed")
+                && savedInstanceState.containsKey("isRunning")
+        ) {
+            timeElapsed = savedInstanceState.getLong("timeElapsed");
+            isRunning = savedInstanceState.getBoolean("isRunning");
+            chronometer.setBase(SystemClock.elapsedRealtime() - timeElapsed);
+        }
 
         butStart.setOnClickListener(v -> {
-            if (timeElapsed == 0) {
-                chronometer.setBase(SystemClock.elapsedRealtime());
-            } else {
-                chronometer.setBase(SystemClock.elapsedRealtime() - timeElapsed);
+            if (!isRunning) {
+                startChronometer();
             }
-            chronometer.start();
+            isRunning = true;
         });
 
         butStop.setOnClickListener(v -> {
-            chronometer.stop();
+            if (isRunning) {
+                stopChronometer(SystemClock.elapsedRealtime() - chronometer.getBase());
+            }
+            isRunning = false;
         });
 
         butReset.setOnClickListener(v -> {
-            chronometer.stop();
-            timeElapsed = 0;
-            chronometer.setBase(SystemClock.elapsedRealtime());
+            isRunning = false;
+            resetChronometer();
         });
+    }
+
+    private void startChronometer() {
+        if (timeElapsed == 0) {
+            chronometer.setBase(SystemClock.elapsedRealtime());
+        } else {
+            chronometer.setBase(SystemClock.elapsedRealtime() - timeElapsed);
+        }
+        chronometer.start();
+    }
+
+    private void resetChronometer() {
+        stopChronometer(0);
+        chronometer.setBase(SystemClock.elapsedRealtime());
+    }
+
+    private void stopChronometer(long timeElapsed) {
+        chronometer.stop();
+        this.timeElapsed = timeElapsed;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (isRunning) {
+            stopChronometer(SystemClock.elapsedRealtime() - chronometer.getBase());
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (isRunning) {
+            startChronometer();
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putLong("timeElapsed", timeElapsed);
+        outState.putBoolean("isRunning", isRunning);
     }
 }
